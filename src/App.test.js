e435df55eee29bcbeb67418e5438f4d23096813f
Grahambone37 <electricgrunt37@gmail.com>
2023-02-24 02:01:45 -0800

    tested text and button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/no-unnecessary-act */
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { act } from "react-dom/test-utils"
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("will h1 render with correct text", () => {
+  render(<App />)
+
+  let headerEI = screen.getByRole('heading')
+  expect(headerEI.textContent).toBe("Testing Playground")
+})
+
+describe('test for the button', () => {
+  test("button background-color is correct",() => {
+    render(<App />)
+    let buttonInQuestion = screen.getByRole('button')
+    
+    act(() => {
+      userEvent.click(buttonInQuestion)
+    })
+    
+    expect(buttonInQuestion).toHaveStyle(` background-color: blue `)
+    expect(buttonInQuestion.textContent).toBe('Change button color to green')
+  })
+})
