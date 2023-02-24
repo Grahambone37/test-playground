de2b24489833b4ef4efabb445b5efb9355ca8238
Grahambone37 <electricgrunt37@gmail.com>
2023-02-24 04:30:11 -0800

    done with subText

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ function App() {
   const [btnColor, setBtnColor] = useState('green')
   const newBtnColor = btnColor === 'green' ? 'blue' : 'green'
 
+  const [btnDisabled, setBtnDisabled] = useState(false)
+
+  const isCheckboxEnabled = btnDisabled ? 'disabled' : 'enabled'
+
   return (
     <div className="App">
       <h1>Testing Playground</h1>
+      <h3>Button is {isCheckboxEnabled}</h3>
       <button
         style={{ backgroundColor: btnColor }}
         onClick={() => setBtnColor(newBtnColor)}
+        disabled={btnDisabled}
       >
         Change button color to {newBtnColor}
       </button>
+      <input type="checkbox" id="disable-button" name="disable-button" onChange={e => setBtnDisabled(e.target.checked) } />
+      <label htmlFor="disable-button">Disable Btn</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ import App from './App'
 test("will h1 render with correct text", () => {
   render(<App />)
 
-  let headerEI = screen.getByRole('heading')
+  let headerEI = screen.getByRole('heading', {name: /Testing Playground/i})
   expect(headerEI.textContent).toBe("Testing Playground")
 })
 
@@ -22,5 +22,45 @@ describe('test for the button', () => {
     
     expect(buttonInQuestion).toHaveStyle(` background-color: blue `)
     expect(buttonInQuestion.textContent).toBe('Change button color to green')
+  })
+})
+
+describe('test for the checkbox', () => {
+  test("checkbox should disable button when clicked", () => {
+    render(<App />)
+    let theBtn = screen.getByRole('button')
+    let checkBox = screen.getByRole('checkbox')
+
+    act(() => {
+      userEvent.click(checkBox)
+    })
+
+    expect(theBtn).toBeDisabled()
+
+    act(() => {
+      userEvent.click(checkBox)
+    })
+
+    expect(theBtn).toBeEnabled()
+  })
+})
+
+describe('is the text changing with the button', () => {
+  test('text changes if button is disabled', () => {
+    render(<App />)
+    let subText = screen.getByRole('heading', { name: /Button is/i })
+    let checkBox = screen.getByRole('checkbox')
+
+    act(() => {
+      userEvent.click(checkBox)
+    })
+
+    expect(subText.textContent).toBe('Button is disabled')
+
+    act(() => {
+      userEvent.click(checkBox)
+    })
+
+    expect(subText.textContent).toBe('Button is enabled')
   })
 })
